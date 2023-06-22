c0935884b58a46212835e293ecf47abf40e9d6f7
Giovanni Vecchies <giovam19@gmail.com>
2023-06-22 22:03:47 +0200

    Structure of db

---
+++ db/squema.sql
@@ -0,0 +1,12 @@
+DROP DATABASE IF EXISTS caida;
+CREATE DATABASE caida;
+USE caida;
+
+DROP TABLE IF EXISTS users CASCADE;
+CREATE TABLE users(
+    email		VARCHAR(255),
+	name			VARCHAR(255),
+    username			VARCHAR(255),
+    password		VARCHAR(255),
+	PRIMARY KEY (email)
+);
